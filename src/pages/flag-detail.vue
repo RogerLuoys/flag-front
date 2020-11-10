0a60e77ee0d3e6a3bbef09178d083ee8338f1104
luoys <luoys@servyou.com.cn>
2020-11-10 19:40:14 +0800

    查询flag详情

--- src/pages/flag-detail.vue
+++ src/pages/flag-detail.vue
@@ -4,7 +4,7 @@
     </el-page-header>
 
     <el-divider>任务A</el-divider>
-    <el-form ref="flagForm" :model="flagForm" label-width="5cm" style="max-width: 1000px">
+    <el-form ref="pageData" :model="pageData" label-width="5cm" style="max-width: 1000px">
       <el-form-item>
         <el-button type="primary" @click="onSubmit"  size="small">保存</el-button>
         <el-button  size="small">完成</el-button>
@@ -13,22 +13,22 @@
         <el-button  size="small">还原</el-button>
       </el-form-item>
       <el-form-item label="业务ID：">
-        <el-input v-model="flagForm.flagID" size="small" maxlength="30" show-word-limit></el-input>
+        <el-input v-model="pageData.flagId" size="small" maxlength="30" show-word-limit></el-input>
       </el-form-item>
       <el-form-item label="名称：">
-        <el-input v-model="flagForm.flagID" size="small"></el-input>
+        <el-input v-model="pageData.flagName" size="small"></el-input>
       </el-form-item>
       <el-form-item label="类型：">
-        <el-radio-group v-model="flagForm.flagID"  size="small">
+        <el-radio-group v-model="pageData.type"  size="small">
           <el-radio label="FLAG"></el-radio>
           <el-radio label="习惯"></el-radio>
         </el-radio-group>
       </el-form-item>
       <el-form-item label="开始时间：">
-        <el-date-picker type="date" placeholder="选择日期" v-model="flagForm.date"  size="small"></el-date-picker>
+        <el-date-picker type="date" placeholder="选择日期" v-model="pageData.startDate"  size="small"></el-date-picker>
       </el-form-item>
       <el-form-item label="结束时间：">
-        <el-date-picker type="date" placeholder="选择日期" v-model="flagForm.date"  size="small"></el-date-picker>
+        <el-date-picker type="date" placeholder="选择日期" v-model="pageData.endDate"  size="small"></el-date-picker>
       </el-form-item>
       <el-form-item label="优先级：">
         <el-select v-model="pageControl.options.value" placeholder="请选择"  size="small">
@@ -41,13 +41,13 @@
         </el-select>
       </el-form-item>
       <el-form-item label="描述：">
-        <el-input v-model="flagForm.flagID" type="textarea" style="max-width: 500px"></el-input>
+        <el-input v-model="pageData.description" type="textarea" style="max-width: 500px"></el-input>
       </el-form-item>
       <el-form-item label="期望目标：">
-        <el-input v-model="flagForm.flagID" type="textarea"></el-input>
+        <el-input v-model="pageData.expected" type="textarea"></el-input>
       </el-form-item>
       <el-form-item label="实际成果：">
-        <el-input v-model="flagForm.flagID" type="textarea"></el-input>
+        <el-input v-model="pageData.actual" type="textarea"></el-input>
       </el-form-item>
       <div style="margin-left: 90px;">
         <el-divider content-position="right">
@@ -55,12 +55,12 @@
           <el-button type="text" size="small">点我新增关联任务</el-button>
         </el-divider>
       </div>
-      <el-table :data="flagForm.table" style="margin-left: 100px">
-        <el-table-column prop="taskID" label="名称" width="150"></el-table-column>
-        <el-table-column prop="taskID" label="简介" width="150"></el-table-column>
-        <el-table-column prop="taskID" label="积分" width="150"></el-table-column>
-        <el-table-column prop="taskID" label="周期" width="150"></el-table-column>
-        <el-table-column prop="taskID" label="操作" width="150">
+      <el-table :data="pageData.tasks" style="margin-left: 100px">
+        <el-table-column prop="taskName" label="名称" width="150"></el-table-column>
+        <el-table-column prop="description" label="简介" width="150"></el-table-column>
+        <el-table-column prop="point" label="积分" width="150"></el-table-column>
+        <el-table-column prop="cycle" label="周期" width="150"></el-table-column>
+        <el-table-column label="操作" width="150">
           <template v-slot="scope">
             <el-button @click="queryDetail(scope.row)" type="text" size="small">查看</el-button>
             <el-button @click="complete(scope.row)" type="text" size="small">完成</el-button>
@@ -76,23 +76,23 @@
 export default {
   data () {
     return {
-      flagForm: {
-        flagID: 'test',
+      pageData: {
+        flagId: 'test',
         flagName: '',
         type: '',
-        startDate: '',
+        startDate: '2020-10-17',
         endDate: '',
         priority: '',
         description: '',
         expected: '',
         actual: '',
-        date: '2020-10-17',
-        table: [
+        tasks: [
           {
-            taskID: 'test2',
-            taskName: '',
+            taskId: 'test2',
+            taskName: 'test2',
             description: '',
             point: '',
+            type: '',
             cycle: ''
           }
         ]
