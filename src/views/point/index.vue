d0da3e9b03eaf11f16fb82b6f4a5406716a48b2b
luoys <luoys@servyou.com.cn>
2021-01-13 22:44:52 +0800

    积分

--- src/views/point/index.vue
+++ src/views/point/index.vue
@@ -34,22 +34,38 @@
 
     <el-drawer title="做点什么奖励自己吧，劳逸结合才是正道" :visible.sync="pageControl.drawerVisible" size="35%">
       <div class="block">
-        <span class="demonstration">幻灯片</span>
         <el-carousel height="120px" :autoplay="false" trigger="click" type="card" arrow="never">
-          <el-carousel-item @click="pageControl.innerDrawerVisible = true" style="background-color: #42b983">
-            <el-image :src="pageControl.shopping" :fit="pageControl.fit"></el-image>
+          <el-carousel-item @click="pageControl.innerDrawerVisible = true">
+            <el-image :src="pageControl.shopping" :fit="pageControl.fit" @click="changeCarousel(1)"></el-image>
           </el-carousel-item>
-          <el-carousel-item @click="pageControl.innerDrawerVisible = true" style="background-color: blue">
-            <el-image :src="pageControl.travel" :fit="pageControl.fit"></el-image>
+          <el-carousel-item @click="pageControl.innerDrawerVisible = true">
+            <el-image :src="pageControl.travel" :fit="pageControl.fit" @click="changeCarousel(2)"></el-image>
           </el-carousel-item>
-          <el-carousel-item @click="pageControl.innerDrawerVisible = true" style="background-color: orange">
-            <el-image :src="pageControl.customize" :fit="pageControl.fit"></el-image>
+          <el-carousel-item @click="pageControl.innerDrawerVisible = true">
+            <el-image :src="pageControl.customize" :fit="pageControl.fit" @click="changeCarousel(3)"></el-image>
           </el-carousel-item>
         </el-carousel>
       </div>
-      <div>
-
-
+      <div v-if="pageControl.selectedCarousel === 1">
+        <div>
+          <el-link type="success">天猫商城</el-link>
+          <el-input-number size="mini" v-model="pageControl.point"></el-input-number>
+        </div>
+        <el-link type="success">淘宝商城</el-link>
+        <el-link type="success">京东商城</el-link>
+        <el-link type="success">网易严选</el-link>
+        <el-link type="success">拼多多</el-link>
+        <el-link type="success">唯品会</el-link>
+        <el-link type="success">线下商场</el-link>
+      </div>
+      <div v-else-if="pageControl.selectedCarousel === 2">
+        把心事留在书山学海，带身躯飘向碧水蓝天
+      </div>
+      <div v-else-if="pageControl.selectedCarousel === 3">
+        你的积分你做主
+      </div>
+      <div v-else>
+        页面异常，请联系管理员
       </div>
     </el-drawer>
 
@@ -72,18 +88,26 @@ export default {
           description: 'description',
           point: 1,
           date: 'date',
-          comment: 'comment',
+          comment: 'comment'
         }]
       },
       pageControl: {
         drawerVisible: false,
         innerDrawerVisible: false,
+        selectedCarousel: 1,
+        point: 1,
         fit: 'fit',
         travel: 'http://118.24.117.181/images/travel.jpg',
         shopping: 'http://118.24.117.181/images/shopping.jpg',
         game: 'http://118.24.117.181/images/game.jpg',
         customize: 'http://118.24.117.181/images/customize.jpg'
       }
+    }
+  },
+  methods: {
+    changeCarousel (item) {
+      this.pageControl.selectedCarousel = item
+      console.info('点击成功' + this.pageControl.selectedCarousel)
     }
   }
 }
